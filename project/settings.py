6302d44509f3adc3c74d9969491f91842a94fa13
Matt Layman <matthewlayman@gmail.com>
2020-10-15 00:30:13 -0400

    Add customer user.

--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -101,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
+AUTH_USER_MODEL = "users.User"
 
 
 # Internationalization
