faad267e2c96f98c22017ee18abf79956a78a5c9
Matt Layman <matthewlayman@gmail.com>
2020-10-22 21:40:36 -0400

    Set the LOGIN_REDIRECT_URL.
    Fixes #7

--- project/settings.py
+++ project/settings.py
@@ -120,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 AUTH_USER_MODEL = "users.User"
+LOGIN_REDIRECT_URL = "core:index"
 
 
 # Internationalization
