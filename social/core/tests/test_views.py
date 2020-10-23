c9ccb7b40d3f4e46f11c0d96287fc17556911aaa
Matt Layman <matthewlayman@gmail.com>
2020-10-22 23:56:02 -0400

    Separate templates for the IndexView.

--- social/core/tests/test_views.py
+++ social/core/tests/test_views.py
@@ -4,3 +4,9 @@ from social.test import TestCase
 class TestIndex(TestCase):
     def test_not_authenticated(self):
         self.get_check_200("core:index")
+
+    def test_ok(self):
+        user = self.make_user()
+
+        with self.login(user):
+            self.get_check_200("core:index")
--- social/core/views.py
+++ social/core/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = "core/index.html"
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return ["core/index.html"]
+        else:
+            return ["core/landing.html"]
